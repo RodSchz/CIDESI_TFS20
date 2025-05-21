323b2556aee5f036a38dfed6023d096fbe5e2908
RodSchz <rsanchezf85@gmail.com>
2025-05-21 12:47:59 -0600

    Carga de archivos
    Carga inicial de archivos de tecnología

---
+++ tech/CIDESI_TFS20/macros/Exportar modelos.rb
@@ -0,0 +1,27 @@
+# $description: Exportar modelos de simulación
+# $version: 0.1
+# $show-in-menu
+module GenerateModels
+ 
+  include RBA
+
+  def self.create_rtd_lib(sheet_rho, output_dir)
+    lib_path = File.join(output_dir, "cidesi_tfs20_models.lib")
+    
+    File.open(lib_path, "w") do |f|
+      f.puts "* Librería de modelos para proceso CIDESI_TFS10"      
+      f.puts "* Autor: Rodolfo Sanchez Fraga"
+      f.puts "* Gerencia de Semiconductores y Microtecnologías, CIDESI"
+      f.puts "* Modelo resistor con efecto de temperatura, Resistencia por cuadro: #{sheet_rho} Ω/□"
+      f.puts ".model RTFS20 RES(TC1=0.00587)"
+      f.puts ".include netlist.cir"
+    end
+    
+    puts "Archivo .lib generado en: #{lib_path}"
+    lib_path
+  rescue => e
+    puts "Error al crear .lib: #{e.message}"
+    nil
+  end
+  
+end
